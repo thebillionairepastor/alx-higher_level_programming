ea7f34f93daadd5acdb8c2e05b77366b9c5cbf81
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-06-30 12:58:11 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    count = len(sys.argv)
-    if count == 1:
-        print("0 argument.")
-    elif count == 2:
-        print("1 argument:")
-    else:
-        print("{:d} arguments:".format(count - 1))
-    for i in range(1, count):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 's:'
+print(argStr.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
