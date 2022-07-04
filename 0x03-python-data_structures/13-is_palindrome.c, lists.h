d8c3e2408fd92833c8789fb80067d027711ff9ae
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-07-04 10:26:02 +0100

    Update 13-is_palindrome.c, lists.h

--- 0x03-python-data_structures/13-is_palindrome.c, lists.h
+++ 0x03-python-data_structures/13-is_palindrome.c, lists.h
@@ -1,41 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * palindrome - check if is palindrome with recursion
- * @l: l
- * @r: r
- *
- * Return: 1 palindrome, 0 not palindrome
+ * is_palindrome - unction in C that checks if a
+ * singly linked list is a palindrome.
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-int palindrome(listint_t **l, listint_t *r)
+int is_palindrome(listint_t **head)
 {
-	int response;
+	int i = 0;
+    int j;
+    int count = 0;
+	listint_t *front, *rear;
 
-	if (r != NULL)
+	while (i != count / 2)
 	{
-		response = palindrome(l, r->next);
-		if (response != 0)
+		front = rear = head;
+		for (j = 0; j < i; j++)
 		{
-			response = (r->n == (*l)->n);
-			*l = (*l)->next;
-			return (response);
+			front = front->next;
 		}
-		return (0);
-
+		for (j = 0; j < count - (i + 1); j++)
+		{
+			rear = rear->next;
+		}
+		if (front->n != rear->n)
+		{
+			return (0);
+		}
+		else
+		{
+			i++;
+		}
 	}
 	return (1);
 }
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: head of linked list
+ * reverse_listint -  function that reverses a linked list.
+ * @head: list header.
  *
- * Return: 1 palindrome, 0 not palindrome
+ * Return: a pointer to the first node of the reversed list.
  */
-int is_palindrome(listint_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
-	if (head == NULL)
-	{
-		return (0);
-	}
-	return (palindrome(head, *head));
+	listint_t *first;
+	listint_t *rest;
+
+	if (*head == NULL)
+		return (NULL);
+	first = *head;
+	rest = first->next;
+	if (rest == NULL)
+		return (NULL);
+	reverse_listint(&rest);
+	first->next->next = first;
+	first->next = NULL;
+	*head = rest;
+	return (*head);
 }
