653a938e78bdd3fccda77da56723d5eaa0943254
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-07-18 12:24:12 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
-    """divides 2 integers and prints the result."""
+    """Returns the division of a by b."""
     try:
         div = a / b
-    except (ZeroDivisionError, FloatingPointError):
+    except (TypeError, ZeroDivisionError):
         div = None
     finally:
         print("Inside result: {}".format(div))
-    return 
+    return (div)
