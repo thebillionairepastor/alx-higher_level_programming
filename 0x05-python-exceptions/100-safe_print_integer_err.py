9b56655c29923070faa748d0df60fd9c427b22b8
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-07-18 12:41:03 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    """prints an integer."""
     import sys
     try:
         print("{:d}".format(value))
-        return True
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return False
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
+    else:
+        return (True)
