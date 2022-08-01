047fe01720a128568fd99ea483e92138bf48e162
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-01 08:38:10 +0100

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is module 6-base_geometry
+
+This module contains one class, based on 5-base-geometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class contains one public instance method:
+    `def area(self)`
+    """
+
+    def area(self):
+        """
+        public method to determine area
+        Raises:
+        Exception with the message
+        """
+        raise Exception("area() is not implemented")
