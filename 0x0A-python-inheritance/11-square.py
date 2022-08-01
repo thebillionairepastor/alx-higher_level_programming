b47d1fd33a3cf288657e5aac345991932bf90b70
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-01 15:28:26 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,44 +1,17 @@
 #!/usr/bin/python3
-"""
-This is module 11-square
-
-This module contains one function
-"""
-
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    inherits class from Rectangle (9-rectangle.py)
-
-    Instantiation with width and height:
-    Instantiation with size: def __init__(self, size)::
-    size must be private. No getter or setter
-    size must be a positive integer, validated by integer_validator
-
-    Args:
-    size
-    Returns:
-    area of shape
-    """
+    """Represent a square."""
 
     def __init__(self, size):
-        """
-        public instantiation
-        """
-        self.__size = size
-        self.__width = size
-        self.__height = size
-        super().__init__(size, size)
+        """Initialize a new square.
 
-    def __str__(self):
+        Args:
+            size (int): The size of the new square.
         """
-        instance method for string - print class name with
-        following rectangle description: [Rectangle] <width>/<height>
-        """
-        return ("[{:s}] {:d}/{:d}".
-                format(__class__.__name__,
-                       self.__width,
-                       self.__height))
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
