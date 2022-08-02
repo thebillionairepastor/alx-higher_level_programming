65f8a0fb9ba3c1c9c18e7c9c7962b5263290da55
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-02 15:54:30 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,13 +1,48 @@
 #!/usr/bin/python3
-Student = __import__('12-student').Student
+"""
+This is module 12-student
 
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
+This module contains one class:
+     `Student`
 
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
+This module contains one function:
+     `to_json`
+"""
 
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
+
+class Student:
+    """
+    Public instance attributes:
+        first_name
+        last_name
+        age
+
+    Public class method
+    def to_json(self)
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age:
+            `def __init__(self, first_name, last_name, age)`
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """public class method
+        def to_json(self, attrs=None): retrieves a dictionary
+        representation of a Student instance
+        (same as 10-class_to_json.py):
+
+        If attrs is a list of strings, only attributes name contain
+        in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            dict_keys = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    dict_keys[item] = getattr(self, item)
+            return (dict_keys)
