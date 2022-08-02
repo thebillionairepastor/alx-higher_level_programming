d8c7e33c81ab7d8aa26bd6bbf19aebc3a18fda8b
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-02 16:15:36 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
+"""Write a function that returns the JSON representation
+   of an object (string):"""
 import json
-"""
-This is module 3-to_json_string
-
-This module contains one function `to_json_string`
-"""
 
 
 def to_json_string(my_obj):
-    """
-    function that returns JSON representation
-    of an object (string):
-    You don't need to manage exceptions if
-    the object can't be serialized.
-
-    Args:
-    my_obj
-
-    Return:
-    JSON representation of object
-    """
+    """Function"""
     return json.dumps(my_obj)
