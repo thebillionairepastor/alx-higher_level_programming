f52e01d0c4c4b005c5f77e6f9b7a9232980d3c9f
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-02 13:15:13 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Module that contains a function that writes to a text file
-"""
+""" Module that contains a function that reads from a file """
 
 
-def write_file(filename="", text=""):
-    """ Function that writes to a text file
+def read_file(filename=""):
+    """ Function that reads from a file
 
     Args:
         filename: filename
-        text: text to write
 
     Raises
         Exception: when the file can be opened
 
     """
 
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
