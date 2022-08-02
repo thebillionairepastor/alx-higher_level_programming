dda6e96872d8cd25997077c0ff45a951ef628575
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-08-02 16:17:45 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""Write a class Student that defines a student by"""
 
 
 class Student:
-    """ Class to create student instances """
-
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """This function init the class"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained in
+        this list must be retriev"""
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for i in attrs:
+            if i in self.__dict__ and type(i) is str:
+                my_dict[i] = self.__dict__.get(i)
+
+        return my_dict
